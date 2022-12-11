55470dbef3bd5a5131f47e415039694a85321a96
AlexxeyEfimov <80613577+AlexxeyEfimov@users.noreply.github.com>
2022-12-11 20:07:36 +1000

    Add files via upload

---
+++ sigm/main.py
@@ -0,0 +1,16 @@
+print('введите первое число')
+
+a = input()
+
+print('введите второе число')
+
+b = input()
+
+a = int(a)
+
+b = int(b)
+
+sum = a + b
+
+print(sum)
+
---
+++ sigm/test.py
@@ -0,0 +1,1 @@
+print('Hello UCHI.ru')
